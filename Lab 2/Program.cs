acbd7168d2290d87ae75be77d753ba608d2eb429
LinusErikson <Linuserikson@gmail.com>
2015-10-12 14:56:53 +0200

    Added task and question 1.1

--- Lab 2/Program.cs
+++ Lab 2/Program.cs
@@ -10,8 +10,31 @@ namespace Lab_2
     {
         static void Main(string[] args)
         {
+            BoilingWater();
         }
+        static void BoilingWater()
+        {
+            //Uppgift 1.1
+            int WaterDegrees = Convert.ToInt32(Console.ReadLine());
+            if (WaterDegrees == 100)
+            {
+                Console.WriteLine("Water is boiling!");
+            }
+            else if (WaterDegrees == 50)
+            {
+                Console.WriteLine("Boiling soon!");
+            }
+            else
+            {
+                Console.WriteLine("I'ts not boiling");
+            }
 
+            //Fråga 1.1
+            // Vad är det för skillnad med att skriva = och ==? När använder vi = och när
+            //använder ==? 
+
+            //'=' tecknet används när man tilldelar värden tex 'sum = 5'. '==' används för att kolla ett villkor tex 'if (input == 5), gör detta..'
+        }
 
     }
 

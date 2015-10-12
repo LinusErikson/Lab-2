ad3ff93b714bda28be416c493fb3528b264438c1
LinusErikson <Linuserikson@gmail.com>
2015-10-12 15:08:26 +0200

    Added task 1.2

--- Lab 2/Program.cs
+++ Lab 2/Program.cs
@@ -10,7 +10,7 @@ namespace Lab_2
     {
         static void Main(string[] args)
         {
-            BoilingWater();
+            
         }
         static void BoilingWater()
         {
@@ -34,6 +34,40 @@ namespace Lab_2
             //använder ==? 
 
             //'=' tecknet används när man tilldelar värden tex 'sum = 5'. '==' används för att kolla ett villkor tex 'if (input == 5), gör detta..'
+        }
+
+        static void PonyRiding()
+        {
+            int Age, Weight;
+
+            Console.WriteLine("What is your age?");
+            Age = Convert.ToInt32(Console.ReadLine());
+
+            if (Age < 12)
+            {
+                Console.WriteLine("You are too young to compete");
+            }
+            else if (Age > 12)
+            {
+                Console.WriteLine("What is your weight");
+                Weight = Convert.ToInt32(Console.ReadLine());
+                if (Weight <= 30)
+                {
+                    Console.WriteLine("A-pony");
+                }
+                else if (Weight <= 50 && Weight > 30 )
+                {
+                    Console.WriteLine("B-pony");
+                }
+                else if (Weight <= 65 && Weight > 50 )
+                {
+                    Console.WriteLine("C-Pony");
+                }
+                else
+                {
+                    Console.WriteLine("There are no ponys for this weight class");
+                }
+            }
         }
 
     }
